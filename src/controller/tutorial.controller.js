c48416a631cbc3e95292a550938a2033ad17bba7
GorelovaElizaveta <gorelova.exceedteam@gmail.com>
2022-04-01 14:48:08 +0300

    fix error

--- src/controller/tutorial.controller.js
+++ src/controller/tutorial.controller.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const db = require("../models");
 
 const Tutorial = db.tutorials;
@@ -9,90 +10,114 @@ exports.create = (req, res) => {
     description,
     published: published ? published : false,
   };
-  Tutorial.create(tutorial).then((data) => {
-    res.send(data);
-  }).catch((err) => {
-    res.status(500).send({
-      message:
-        err.message || `Some error occurred while create.`,
-    });
-  });
-};
-
-exports.findAll = (req, res) => {
-  Tutorial.findAll().then((data) => {
-    res.send(data);
-  }).catch((err) => {
-    res.status(500).send({
-      message:
-        err.message || `Receive error.`,
-    });
-  });
-};
-
-exports.findOne = (req, res) => {
-  const { id } = req.params;
-  Tutorial.findByPk(id).then((data) => {
-    res.send(data);
-  }).catch((err) => {
-    res.status(500).send({
-      message:
-        err.message || `Error getting one record.`,
-    });
-  });
-};
-
-exports.updateTitle = (req, res) => {
-  const { id } = req.params;
-  const { title } = req.query;
-  let presentTitle = req.query.hasOwnProperty("title");
-
-  if (presentTitle) {
-    Tutorial.update(
-      { title: title },
-      {
-        where: { id: id },
-      }
-    ).then((data) => {
-      Tutorial.findAll().then((data) => {
-        res.send(data);
-      });
-    });
-  } else {
-    res.status(500).send("404 update not created");
-  }
-};
-
-exports.findAllTitle = (req, res) => {
-  const title = req.query.title;
-  Tutorial.findAll({ where: { title } })
+  Tutorial.create(tutorial)
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || `Some error occurred while retrieving tutorials.`,
+        message: err.message || `Some error occurred while create.`,
       });
     });
+};
+
+exports.findAll = (req, res) => {
+  Tutorial.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Receive error.`,
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const { id } = req.query;
+  let presentId = req.query.hasOwnProperty("id");
+  if (id) {
+    Tutorial.findByPk(id)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || `Error getting one record.`,
+        });
+      });
+  } else {
+    res.send("id not found");
+  }
+};
+
+exports.updateTitle = (req, res) => {
+  const body = req.body;
+  const { id, title, description, published } = req.body;
+
+  const sort = req.query.sort;
+  let checkSort = req.query.hasOwnProperty("sort");
+
+  if (body) {
+    Tutorial.update({ title, description, published }, { where: { id } })
+      .then((data) => {
+        Tutorial.findAll({ order: [["id"]] })
+          .then((data) => {
+            res.send(data);
+          })
+          .catch((err) => {
+            res.status(500).send({
+              message: err.message || `Error getting task.`,
+            });
+          });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || `Error getting  all task.`,
+        });
+      });
+  } else {
+    res.send("to update you need to get all the data");
+  }
+};
+
+exports.findAllTitle = (req, res) => {
+  const title = req.query.title;
+  let presentTitle = req.query.hasOwnProperty("title");
+
+  if (presentTitle) {
+    Tutorial.findAll({ where: { title } })
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || `Some error occurred while retrieving tutorials.`,
+        });
+      });
+  } else {
+    res.send("title not found");
+  }
 };
 
 exports.remove = (req, res) => {
   const { id } = req.query;
 
   let present = req.query.hasOwnProperty("id");
-  if(present){
-  Tutorial.destroy({ where: { id: id } }).then((data) => {
-    Tutorial.findAll().then((data) => {
-      res.send(data);
-    }).catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || `Error while deleting record.`,
-      });
-    });;
-  });
-} else {
-  res.status(500).send("deletion is not possible");
-}
+  if (present) {
+    Tutorial.destroy({ where: { id: id } }).then((data) => {
+      Tutorial.findAll()
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message || `Error while deleting record.`,
+          });
+        });
+    });
+  } else {
+    res.status(500).send("deletion is not possible");
+  }
 };
